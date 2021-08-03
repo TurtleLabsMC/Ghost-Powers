d6f9c5b706dc827533955f0b6ef6874cd3960264
GGAlex <alexander.katzki@outlook.com>
2021-08-03 17:17:40 +0200

    Added Ghost Status Effect

---
+++ src/main/java/com/github/turtlelabsmc/ghostpowers/entity/effect/GhostStatusEffect.java
@@ -0,0 +1,35 @@
+package com.github.turtlelabsmc.ghostpowers.entity.effect;
+
+import com.github.turtlelabsmc.ghostpowers.GhostPowers;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.AttributeContainer;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
+
+public class GhostStatusEffect extends StatusEffect {
+    public GhostStatusEffect() {
+        super(
+                StatusEffectType.BENEFICIAL,
+                0xFFFFFF);
+    }
+
+    @Override
+    public void onApplied(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+        if(entity instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) entity;
+            player.world.playSound(null, player.getBlockPos(), GhostPowers.REAPERS_BELL_RING, SoundCategory.PLAYERS, 1f, 1f);
+            //Todo: Add to Player Ghost Logic
+        }
+    }
+
+    @Override
+    public void onRemoved(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+        if (entity instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) entity;
+            player.world.playSound(null, player.getBlockPos(), GhostPowers.REAPERS_BELL_RING, SoundCategory.PLAYERS, 1f, 1f);
+            //Todo: Remove Ghost Logic from Player
+        }
+    }
+}
